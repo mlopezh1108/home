b57f31c21cdbbd9eec2a1215cd485c9d13d01d0a
Marcos Lopez <mlopez@palo-it.com>
2024-01-07 21:27:26 -0600

    corrigiendo estilos

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -5,24 +5,4 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-  scrollPosition: number = 0;
-  listener!: any;
-
-  ngOnInit() {
-    document.addEventListener('scroll', this.#eventHandler);
-  }
-
-  ngOnDestroy(): void {
-    document.removeEventListener('scroll', this.#eventHandler);
-  }
-
-  #eventHandler = () => this.togglePicture();
-
-  private togglePicture(): void {
-    const img = document.getElementById('profilePicture');
-    if (window.scrollY > this.scrollPosition) img!.style.display = 'none';
-    if (window.scrollY === 0) img!.style.display = 'inline';
-    this.scrollPosition = window.scrollY;
-  }
-}
+export class HeaderComponent {}
