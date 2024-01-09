e685268e2fdd946b8ac1da0dce574a949fc54ec3
Marcos Lopez <mlopez@palo-it.com>
2024-01-08 22:36:35 -0600

    v0.0.27

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,12 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: ''
-  }
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
