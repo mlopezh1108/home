bd8e767866f6b778bce26a16bb6a8bd7a55aee12
Marcos Lopez <mlopez@palo-it.com>
2024-01-12 00:57:01 -0600

    Creando script para generar build

---
+++ build.script.mjs
@@ -0,0 +1,33 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import { default as Prompt } from 'prompt-sync';
+
+const prompt = Prompt();
+const PACKAGE = './package.json';
+
+start();
+
+function start() {
+  if (!treeClean()) {
+    console.warn('Aún tiene cambios pendientes, por favor haga commit');
+    return;
+  }
+  const { version } = JSON.parse(readFileSync(PACKAGE).toString('utf-8'));
+  execSync('ng build');
+  execSync('git checkout release');
+  execSync('git add docs/.');
+  execSync(`git commit -m "v${version}"`);
+  execSync(`git tag -a v${version}`);
+  execSync(`git push`);
+  execSync(`git push origin v${version}`);
+}
+
+function treeClean() {
+  try {
+    const result = execSync('git status');
+    return result.toString('utf-8').includes('working tree clean');
+  } catch (error) {
+    console.error(error);
+  }
+  return false;
+}
